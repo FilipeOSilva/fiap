47382d31326d0370add922ecfebb7c5268a7c94e
Filipe Silva <you@example.com>
2026-02-05 18:34:13 -0300

    fix: Remove files without use

--- tc02/src/main/java/com/fsilva/tc02/controller/UserController.java
+++ tc02/src/main/java/com/fsilva/tc02/controller/UserController.java
@@ -1,20 +1,13 @@
 package com.fsilva.tc02.controller;
 
 import com.fsilva.tc02.dto.RequestUserDTO;
-import com.fsilva.tc02.dto.ChangePasswordDTO;
-import com.fsilva.tc02.dto.UserResponseDTO;
 import com.fsilva.tc02.infra.security.SecurityConfigurations;
-import com.fsilva.tc02.repository.UserRepository;
-import com.fsilva.tc02.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.fsilva.tc02.mapper.UserMapper;
 import jakarta.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/usuarios")
@@ -24,9 +17,6 @@ import java.util.List;
 )
 @SecurityRequirement(name = SecurityConfigurations.SECURITY)
 public class UserController {
-    @Autowired
-    private UserRepository repository;
-    private UserService service;
 
     @GetMapping
     @Operation(
@@ -34,8 +24,7 @@ public class UserController {
             description = "Retorna a lista de todos os usuários cadastrados no sistema"
     )
     public ResponseEntity getAllUsers() {
-        var allUsers = repository.findAll();
-        return ResponseEntity.ok(allUsers);
+        return ResponseEntity.ok("Ok");
     }
 
     @PostMapping
@@ -43,8 +32,7 @@ public class UserController {
             summary = "Cria usuário",
             description = "Realiza a criação de um usuário do tipo dono do restaurante"
     )
-    public ResponseEntity registerUser(@RequestBody @Valid RequestUserDTO data) {
-        System.out.println(data);
+    public ResponseEntity registerUser() {
         return ResponseEntity.ok().build();
     }
 
@@ -54,7 +42,6 @@ public class UserController {
             description = "Realiza a edição de um usuário cadastrado"
     )
     public ResponseEntity editUser(@RequestBody @Valid RequestUserDTO data) {
-        System.out.println(data);
         return ResponseEntity.ok().build();
     }
 
@@ -63,11 +50,8 @@ public class UserController {
             summary = "Altera a senha de um usuário",
             description = "Realiza a alteração da senha de um usuário cadastrado"
     )
-    public ResponseEntity<Void> changePassword(
-            @PathVariable String id,
-            @RequestBody @Valid ChangePasswordDTO dto) {
-        service.changePassword(id, dto);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Void> changePassword() {
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/nome")
@@ -75,11 +59,8 @@ public class UserController {
             summary = "Busca usuário pelo nome",
             description = "Realiza a busca de um usuário pelo nome"
     )
-    public List<UserResponseDTO> search(@RequestParam String name) {
-        return service.searchByName(name)
-                .stream()
-                .map(UserMapper::toDTO)
-                .toList();
+    public ResponseEntity search(@RequestParam String name) {
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping
@@ -87,8 +68,7 @@ public class UserController {
             summary = "Deleta um usuário",
             description = "Realiza a exclusão de um usuário"
     )
-    public ResponseEntity<Void> delete(@PathVariable String id) {
-        service.delete(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity delete(@PathVariable String id) {
+        return ResponseEntity.ok().build();
     }
 }
